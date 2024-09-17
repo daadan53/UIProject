fd382148b7886a05daea78d5220c2caeae5bf4ff
daadan53 <dadan53unity@gmail.com>
2024-09-17 11:11:00 +0200

    Update Test

--- Assets/Tests/TestUI.cs
+++ Assets/Tests/TestUI.cs
@@ -3,17 +3,27 @@ using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
+using UnityEngine.UIElements;
 
+[TestFixture]
 public class TestUI
 {
     // Test simple genre vrai ou faux --> EditMod ?
     [Test]
     public void TestUISimplePasses()
     {
-        // Use the Assert class to test conditions
+        // Arrange
         var go = new GameObject();
         var uiMana = go.AddComponent<UI_Mana>();
+ 
+        var uiDocument = go.AddComponent<UIDocument>();
+        var visualTree = Resources.Load<VisualTreeAsset>("UI/NewUXMLTemplate"); // Charge le fichier UXML depuis les ressources
 
+        // Act
+        uiDocument.visualTreeAsset = visualTree;  // Associez le fichier UXML à l'UIDocument
+        uiMana._Root = uiDocument.rootVisualElement;
+
+        // Assert
         Assert.IsNotNull(uiMana._Root);
     }
 
